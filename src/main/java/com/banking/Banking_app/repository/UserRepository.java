ff5734c1a7c9e6d3140610498c66bce01527ffdd
dev-contexIq <ruturaj.jadhav-ext.citiuscloud.com>
2024-11-26 22:59:38 +0530

    t

--- src/main/java/com/banking/Banking_app/repository/UserRepository.java
+++ src/main/java/com/banking/Banking_app/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.banking.Banking_app.repository;
 
 import com.banking.Banking_app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserRepository extends JpaRepository<User , Long> {
 }
